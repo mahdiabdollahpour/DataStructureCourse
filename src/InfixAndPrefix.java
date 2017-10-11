44b23b904df7b4572145ee6e8921f75116c45993
mohammadmahdi abdollahpour <mohammadmahdiabdollahpoor@gmail.com>
2017-10-11 22:47:39 +0330

    working on prefix and infix

--- src/InfixAndPrefix.java
+++ src/InfixAndPrefix.java
@@ -3,7 +3,15 @@ import java.util.Stack;
 /**
  * Created by ASUS on 10/10/2017.
  */
+
+/**
+ * This algorithm maintains two stacks. 1st stack for all operators and 2nd stack to store the operands.
+ * 1)  Validate the Infix Expression for correctness
+ * a) ‘(‘ and ‘)’ are in pairs
+ * b) Operator is in between 2 operands (Binary operators are considered only)
+ */
 public class InfixAndPrefix {
+
     public static boolean isNum(char c) {
         int ascii = (int) c;
         if (ascii < 48) {
@@ -24,76 +32,70 @@ public class InfixAndPrefix {
 
     public static int precedense(char c) {
         if (c == '*' || c == '/') {
-            return 3;
+            return 7;
         } else if (c == '+' || c == '-') {
-            return 5;
+            return 3;
         }
-        return 100;
-    }
-//
-//
-//    public static void infixToPrefix(String input) {
-//        Stack<Character> s = new Stack<>();
-//        // s.add(')');
-//        //   input = input.concat("(");
-//        for (int i = input.length() - 1; i >= 0; i--) {
-//            char c = input.charAt(i);
-//            if (isNum(c)) {
-//                System.out.print(c);
-//            }
-////            else if (c == ')') {
-////                s.add(c);
-////            }
-//            else if (isOp(c)) {
-//                if (!s.empty()) {
-//                    char temp = s.pop();
-//                    while (precedense(temp) >= precedense(c) && !s.empty()) {
-//                        System.out.println(temp);
-//                        temp = s.pop();
-//                    }
-//                    s.add(temp);
-//                }
-//                s.add(c);
-//            }
-////            else if (c == '(') {
-////                char temp = s.pop();
-////                while (temp != '(' && !s.empty()) {
-////                    System.out.println(temp);
-////                    temp = s.pop();
-////                }
-////            }
-//
-//
-//        }
-//        while (!s.empty()) {
-//            char a = s.pop();
-//            System.out.print(a);
-//        }
-//
-//
-//    }
-
-    public static String reverse(String input) {
-        StringBuilder sb = new StringBuilder(input.length());
-        for (int i = input.length() - 1; i >= 0; i--) {
-            sb.append(input.charAt(i));
-        }
-        return sb.toString();
+        return 0;
     }
 
-    public static String infixToPreReversing(String input) {
-        String reversed = reverse(input);
 
-        String postfix = InfixAndPostfix.infixToPostfix(reversed);
+    public static String inToPre(String input) {
+        Stack<Character> operatorStack = new Stack<>();
+        Stack<String> numbersStack = new Stack<>();
+        operatorStack.add('(');
+        input = input.concat(")");
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
 
-        return reverse(postfix);
+
+            if (c == '(') {
+                operatorStack.add(c);
+            } else if (c == ')') {
+
+                char OPERATOR = operatorStack.pop();
+
+                while (OPERATOR != '(') {
+                    String OPERAND1 = numbersStack.pop();
+                    String OPERAND2 = numbersStack.pop();
+                    String newElement = Character.toString(OPERATOR).concat(OPERAND1).concat(OPERAND2);
+                    numbersStack.add(newElement);
+                    OPERATOR = operatorStack.pop();
+
+                }
+            } else if (isOp(c)) {
+                char op = operatorStack.pop();
+                boolean higherOrEqualPrecedense = precedense(op) >= precedense(c);
+                while (higherOrEqualPrecedense) {
+
+                    String OPERAND1 = numbersStack.pop();
+                    String OPERAND2 = numbersStack.pop();
+                    String newElement = Character.toString(op).concat(OPERAND1).concat(OPERAND2);
+                    numbersStack.add(newElement);
+                    op = operatorStack.pop();
+                    higherOrEqualPrecedense = precedense(op) >= precedense(c);
+
+
+                }
+                operatorStack.add(op);
+                operatorStack.add(c);
+
+
+            } else {
+                numbersStack.add(Character.toString(c));
+            }
+
+
+        }
+        return numbersStack.pop();
+
 
     }
 
 
     public static void main(String[] args) {
 
-        System.out.println(infixToPreReversing("2+3*4"));
-
+        //    System.out.println(infixToPreReversing("((3+4)*(6+7))/9"));
+        System.out.println(inToPre("(2+3)*(5-1)"));
     }
 }
